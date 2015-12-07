941014aa7efd14ab9539fc86e69a559188006f71
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-12-07 13:39:55 +0900

    modifyq everything

--- rendezvous/app.js
+++ rendezvous/app.js
@@ -1,3 +1,12 @@
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null) {
+    return null;
+  } else{
+    return results[1] || 0;
+  }
+}
+
 function show_map(latitude, longitude) {
   var canvas = document.getElementById("map_canvas");
   var latlng = new google.maps.LatLng(latitude, longitude);
@@ -24,12 +33,31 @@ function watch_position_error(error) {
 
 var options = {
   enableHighAccuracy: false,
-  timeout: 5000,
+  timeout: 10000,
   muximumAge: 0
 };
 
+var friends = [];
+
+function add_friend(id) {
+  friends.push(id);
+}
+
+function initialize_friends() {
+  var paren = $.urlParam('parent');
+  if (paren != null) {
+    add_friend(paren);
+  }
+}
+
 function initialize() {
-  var peer = new Peer({key: 'okvv36v2srdon7b9'});
-  alert(peer);
   var id = navigator.geolocation.watchPosition(watch_position_success, watch_position_error, options);
+
+  var peer = new Peer({key: 'okvv36v2srdon7b9'}); 
+  peer.on('open', function(id) {
+    $("#share").html(window.location.href + "?parent=" + id);
+  });
+  var conn = peer.on('connection', function(conn) {
+    add_friend(conn.peer);
+  });
 };
