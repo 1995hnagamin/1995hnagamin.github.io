a8e554b0ef917832f079aa50afa19cd3a0279e99
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-12-07 14:38:28 +0900

    split js

--- rendezvous/app.js
+++ rendezvous/app.js
@@ -7,52 +7,8 @@ $.urlParam = function(name){
   }
 }
 
-function show_map(latitude, longitude) {
-  var canvas = document.getElementById("map_canvas");
-  var latlng = new google.maps.LatLng(latitude, longitude);
-  var options = {
-    zoom: 16,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(canvas, options);
-  var marker = new google.maps.Marker({
-    position: latlng,
-    map: map
-  });
-}
-
-function watch_position_success(position) {
-  var coord = position.coords;
-  show_map(coord.latitude, coord.longitude);
-};
-
-function watch_position_error(error) {
-  alert("ERROR: " + error.message);
-}
-
-var options = {
-  enableHighAccuracy: false,
-  timeout: 10000,
-  muximumAge: 0
-};
-
-var friends = [];
-
-function add_friend(id) {
-  friends.push(id);
-}
-
-function initialize_friends() {
-  var paren = $.urlParam('parent');
-  if (paren != null) {
-    add_friend(paren);
-  }
-}
-
 function initialize() {
   var id = navigator.geolocation.watchPosition(watch_position_success, watch_position_error, options);
-
   var peer = new Peer({key: 'okvv36v2srdon7b9'}); 
   peer.on('open', function(id) {
     $("#share").html(window.location.href + "?parent=" + id);
---
+++ rendezvous/location.js
@@ -0,0 +1,49 @@
+var markers = {};
+var map = null;
+
+function initialize_map(coords) {
+  var canvas = document.getElementById("map_canvas");
+  var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+  var options = {
+    zoom: 16,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(canvas, options);
+  register_marker("@me", coords);
+}
+
+function register_marker(id, coords) {
+  var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+  });
+  markers[id] = marker;
+}
+
+function update_marker(id, coords) {
+  var latlng = new google.maps.LatLng(coords.latitude, coords.longitude);
+  var new_opts = {
+    position: latlng
+  };
+  markers[id].setOptions(new_opts);
+}
+
+function watch_position_success(position) {
+  var coords = position.coords;
+  if (map == null) {
+    initialize_map(coords);
+  }
+  update_marker("@me", coords);
+};
+
+function watch_position_error(error) {
+  alert("ERROR: " + error.message);
+}
+
+var options = {
+  enableHighAccuracy: false,
+  timeout: 10000,
+  muximumAge: 0
+};
