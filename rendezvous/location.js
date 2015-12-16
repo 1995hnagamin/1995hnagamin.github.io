d3bd7fa47ee1c021240454bbcebeb7b10285a5d3
Hideaki Nagamine <1995.hnagamin@gmail.com>
2015-12-17 04:53:34 +0900

    remove frequent alert

--- rendezvous/location.js
+++ rendezvous/location.js
@@ -34,7 +34,6 @@ function update_marker(id, coords) {
 }
 
 function watch_position_success(position) {
-  alert("position: " + JSON.stringify(position));
   var coords = position.coords;
   if (map == null) {
     initialize_map(coords);
